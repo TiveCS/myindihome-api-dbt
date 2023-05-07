e657104571fae46a3ef78be0e213b7102b7f3272
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-07 08:27:30 +0700

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import * as dotenv from "dotenv";
-import { TestController } from "./Controllers/TestController.js";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-
-const testController = new TestController();
-
-app.get("/", (req: Request, res: Response) => {
-  testController.test(req, res);
-});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
