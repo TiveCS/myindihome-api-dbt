2fc72420a6d0836453282b97a998e564d67a5c6a
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-07 10:22:43 +0700

    reset

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import express, { Express, Request, Response } from "express";
 import * as dotenv from "dotenv";
+import express, { Express } from "express";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
-const app: Express = express();
+const createServer = () => {
+  const server: Express = express();
+
+  server.use(bodyParser.json());
+
+  return server;
+};
+
+const server: Express = createServer();
 const port = process.env.PORT;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
